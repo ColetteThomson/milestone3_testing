455d4899a64ff1865071d0760e8a889397ccd54c
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-09-20 08:56:44 +0000

    produce terminal message

--- run.py
+++ run.py
@@ -12,11 +12,26 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('spreadsheet1')
 
-sales = SHEET.worksheet('sales')
+# next 3 lines used to check API is working:
+# sales = SHEET.worksheet('sales')
+# data = sales.get_all_values()
+# print(data)
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    get sales figures input from the user
+    """
+    print('Please enter sales data from the last market.')
+    print('Data should be six numbers, separated by commas.')
+    print('Example: 10, 20, 30, 40, 50, 60\n')
 
-print(data)
+    data_str = input('Enter your data here: ')
+    """ to check this is working..."""
+    print(f"The data provided is {data_str}")
+
+    """ need to call get_sales_data() function... then check using python3 run.py"""
+
+get_sales_data()
 
 
 
