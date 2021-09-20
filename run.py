53f3c7bb3c44c66ff32e4ea60955d65a706a843d
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-09-20 10:23:39 +0000

    validator testing part 2

--- run.py
+++ run.py
@@ -42,11 +42,12 @@ def validate_data(values):
     raises ValueError, if strings cannot be converted into int,
     or if there aren't exactly 6 values
     """
-    """print(values)  """
+    """print(values)"""
 
     """use below to check validation, try entering just 3 values to test. 
     Note 'e' is python shorthand for 'error'"""
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
