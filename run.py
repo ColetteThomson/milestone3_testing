761bdb20ded7bc0983daeec9740d3b417fcf7f6c
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-09-20 10:01:46 +0000

    add validation check

--- run.py
+++ run.py
@@ -26,11 +26,37 @@ def get_sales_data():
     print('Example: 10, 20, 30, 40, 50, 60\n')
 
     data_str = input('Enter your data here: ')
-    """ to check this is working..."""
+    """ to check this is working...
     print(f"The data provided is {data_str}")
+    """
+    """do this after 'calling get_sales_data() function section.
+    use to split entered data string into comma separated value, then print to check """
+    sales_data = data_str.split(",")
+    """print(sales_data)"""
+    validate_data(sales_data)
 
-    """ need to call get_sales_data() function... then check using python3 run.py"""
+"""to validate user input - use print(values) below to check"""
+def validate_data(values):
+    """
+    inside the try, converts all string values into integers.
+    raises ValueError, if strings cannot be converted into int,
+    or if there aren't exactly 6 values
+    """
+    """print(values)  """
 
+    """use below to check validation, try entering just 3 values to test. 
+    Note 'e' is python shorthand for 'error'"""
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+    """ need to call get_sales_data() function... then check using python3 run.py ... 
+    and enter random data"""
 get_sales_data()
 
 
